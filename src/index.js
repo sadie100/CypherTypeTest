6e9f48f4d2e7f6befa5fee5c1cc574643ea8847f
김세희 <82367039+sadie100@users.noreply.github.com>
2022-02-05 22:31:27 +0900

    테스트 페이지 넘어가도록 구현, 리덕스 설정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Main from "./pages/Main";
 import Test from "pages/Test";
 import { ThemeProvider } from "@mui/material/styles";
@@ -10,22 +15,27 @@ import GlobalStyle from "styles/GlobalStyles";
 import "assets/font/font.css";
 import { StylesProvider } from "@mui/styles";
 import Container from "components/base/Container";
+import { store } from "store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
     <StylesProvider injectFirst>
-      <Router>
-        <Switch>
-          <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <CssBaseline />
-            <Container>
-              <Route exact path="/" component={Main} />
-              <Route path="/test" component={Test} />
-            </Container>
-          </ThemeProvider>
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <ThemeProvider theme={theme}>
+              <GlobalStyle />
+              <CssBaseline />
+              <Container>
+                <Route exact path="/" component={Main} />
+                <Route path="/test/:qs" component={Test} />
+                <Redirect path="/test" to="/test/1" />
+              </Container>
+            </ThemeProvider>
+          </Switch>
+        </Router>
+      </Provider>
     </StylesProvider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "components/base/Header";
-import Page1 from "pages/test/Page1";
 import { styled } from "@mui/styles";
 import { QuestionData } from "pages/test/QuestionData";
 import { NormalText } from "components/styledComponent/TextDiv";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "store/pointSlice";
 
 const Wrapper = styled("div")({
   zIndex: "200",
@@ -15,13 +16,44 @@ const Wrapper = styled("div")({
   width: "100%",
   height: "100%",
 });
+const Question = styled("div")({
+  color: "white",
+  fontFamily: "nanumBarun",
+  fontSize: "1rem",
+  textAlign: "left",
+});
+const Answer = styled("div")({
+  color: "white",
+  fontFamily: "nanumBarun",
+  fontSize: "1rem",
+  textAlign: "left",
+});
 
-const Test = () => {
+const Test = (props) => {
+  const { match, history } = props;
+  //url 파라미터로 질문 번호 받음
+  const qsNum = Number(match.params.qs);
+  //리덕스 툴킷으로 point 저장
+  const point = useSelector((state) => state.point.value);
+  const dispatch = useDispatch();
+
+  const handleClickAns = ({ currentTarget }) => {
+    console.log(currentTarget.id);
+    history.push(`/test/${qsNum + 1}`);
+  };
+
   return (
     <>
       <Header />
       <Wrapper>
-        <NormalText>1. 하..금요일..</NormalText>
+        <Question>{QuestionData()[qsNum].quiz}</Question>;
+        {QuestionData()[qsNum].ans.map((select, idx) => {
+          return (
+            <Answer index={idx} id={idx} onClick={handleClickAns}>
+              {select}
+            </Answer>
+          );
+        })}
       </Wrapper>
     </>
   );
