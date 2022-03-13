27ed0c053932cb7037c5c2b2353a58f18431272d
김세희 <82367039+sadie100@users.noreply.github.com>
2022-03-13 21:17:53 +0900

    kakaotalk api 연결

--- src/index.js
+++ src/index.js
@@ -17,7 +17,15 @@ import { StylesProvider } from "@mui/styles";
 import Container from "components/base/Container";
 import { store } from "store";
 import { Provider } from "react-redux";
+import { Result } from "pages/Result";
 
+// const k_key = "aa16d24db6390013864861c4ec278c95";
+// if (!window.Kakao.isInitialized()) {
+//   // JavaScript key를 인자로 주고 SDK 초기화
+//   window.Kakao.init(k_key);
+//   // SDK 초기화 여부를 확인하자.
+//   console.log(window.Kakao.isInitialized());
+// }
 ReactDOM.render(
   <React.StrictMode>
     <StylesProvider injectFirst>
@@ -30,6 +38,7 @@ ReactDOM.render(
               <Container>
                 <Route exact path="/" component={Main} />
                 <Route path="/test/:qs" component={Test} />
+                <Route path="/result" component={Result} />
                 {/* <Redirect path="/test" to="/test/1" /> */}
               </Container>
             </ThemeProvider>
